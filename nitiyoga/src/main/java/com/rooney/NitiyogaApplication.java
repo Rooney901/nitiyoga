a53d65a92e58efe5043d55f811b4381035bd0650
Rooney901 <raunak.gupta901@gmail.com>
2025-12-21 18:51:56 +0530

    Home tab on Users page and componentScan issues fixed.

--- nitiyoga/src/main/java/com/rooney/NitiyogaApplication.java
+++ nitiyoga/src/main/java/com/rooney/NitiyogaApplication.java
@@ -1,9 +1,9 @@
-package com.rooney.nitiyoga;
+package com.rooney;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.rooney.niti.*")
+@SpringBootApplication
 public class NitiyogaApplication {
 
 	public static void main(String[] args) {
